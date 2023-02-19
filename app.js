473764e533e3a038b156bd99e1712f6c358266af
Kiril Yanev <kl_yanev@abv.bg>
2023-02-19 21:55:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,23 +2,45 @@ let randomNumber = Math.floor(Math.random() * 100);
 const inputElement = document.querySelector('#input');
 const guessesElement = document.querySelector('#guesses');
 const winnerElement = document.querySelector('#win');
+const errorElement = document.querySelector('#error');
+const repeatElement = document.querySelector('#repeat');
 const submitBtn = document.querySelector('#submit');
 console.log(randomNumber);
 submitBtn.addEventListener('click',checkGuess);
+let guesses = [];
 
 function checkGuess() {
+    repeatElement.style.display = 'none';
+    errorElement.style.display = 'none';
+
+    let input = Number(inputElement.value);
     let symbol = '';
-    if(randomNumber > inputElement.value) {
+
+    if(input < 0 || input > 100) {
+        errorElement.style.display = 'block';
+        return;
+    }
+
+    if(guesses.includes(input)) {
+        repeatElement.style.display = 'block';
+        return;
+    }
+    if(randomNumber > input) {
         symbol = '↑';
-    } else if (randomNumber < inputElement.value) {
+    } else if (randomNumber < input) {
         symbol = '↓';
-    } else if (randomNumber == inputElement.value) {
+    } else if (randomNumber == input) {
         submitBtn.disabled = true;
         winnerElement.style.display = 'block';
         inputElement.value = '';
         return;
     }
 
-    guessesElement.textContent += `${inputElement.value} ${symbol}`;
+    guesses.push(input,symbol);
+    guessesElement.textContent = guesses.join(' ');
     inputElement.value = '';
+}
+
+function inputValidation(input) {
+    
 }
